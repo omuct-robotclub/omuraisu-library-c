b74d0c0d0bc1bd677e9e564f5fd924d91d2c26fc
bit <mushbeen25@gmail.com>
2026-04-06 18:19:16 +0900

    feat: rewrite dji impl

---
+++ src/dji/robomas.c
@@ -0,0 +1,77 @@
+#include "robomas.h"
+
+Robomas om_rm_init(CanBus* can) {
+  Robomas rm;
+  rm.can = can;
+  rm.core = om_rm_core_init();
+  return rm;
+}
+
+void om_rm_set_max_output(Robomas* rm, int16_t max) {
+  om_rm_core_set_max_output(&rm->core, max);
+}
+
+int om_rm_read(Robomas* rm) {
+  CanMessage msg;
+  if (rm->can->read(rm->can->impl, &msg)) {
+    return om_rm_core_parse(&rm->core, msg.id, msg.data);
+  }
+  return -1;
+}
+
+int om_rm_parse(Robomas* rm, uint32_t id, const uint8_t data[8]) {
+  return om_rm_core_parse(&rm->core, id, data);
+}
+
+void om_rm_set_output(Robomas* rm, int16_t current, int id) {
+  om_rm_core_set_output(&rm->core, current, id);
+}
+
+void om_rm_set_output_percent(Robomas* rm, float percent, int id) {
+  om_rm_core_set_output_percent(&rm->core, percent, id);
+}
+
+bool om_rm_write(Robomas* rm) {
+  CanMessage msg1, msg2;
+  msg1.id = TX_ID_GROUP1;
+  msg2.id = TX_ID_GROUP2;
+  om_rm_core_get_output_group(&rm->core, msg1.data, 0);
+  om_rm_core_get_output_group(&rm->core, msg2.data, 1);
+  msg1.len = 8;
+  msg2.len = 8;
+  bool success1 = rm->can->write(rm->can->impl, &msg1);
+  bool success2 = rm->can->write(rm->can->impl, &msg2);
+  return success1 && success2;
+}
+
+int16_t om_rm_get_current(const Robomas* rm, int id) {
+  return om_rm_core_get_current(&rm->core, id);
+}
+
+uint16_t om_rm_get_angle(const Robomas* rm, int id) {
+  return om_rm_core_get_angle(&rm->core, id);
+}
+
+int16_t om_rm_get_rpm(const Robomas* rm, int id) {
+  return om_rm_core_get_rpm(&rm->core, id);
+}
+
+uint8_t om_rm_get_temp(const Robomas* rm, int id) {
+  return om_rm_core_get_temp(&rm->core, id);
+}
+
+RobomasData om_rm_get_data(const Robomas* rm, int id) {
+  return om_rm_core_get_data(&rm->core, id);
+}
+
+const RobomasData* om_rm_get_data_const(const Robomas* rm, int id) {
+  return &rm->core.data_[id - 1];
+}
+
+RobomasCore om_rm_get_core(const Robomas* rm) {
+  return rm->core;
+}
+
+const RobomasCore* om_rm_get_core_const(const Robomas* rm) {
+  return &rm->core;
+}
---
+++ src/dji/robomas_core.c
@@ -0,0 +1,127 @@
+#include <stdint.h>
+#include <string.h>
+#include "robomas_core.h"
+
+const uint16_t M3508_GEAR_RATIO = 19;
+const uint16_t M2006_GEAR_RATIO = 36;
+const uint16_t ANGLE_MAX_VALUE = 8192;
+
+static const uint32_t RX_ID_MIN = 0x201;
+static const uint32_t RX_ID_MAX = 0x208;
+
+const uint32_t TX_ID_GROUP1 = 0x200;
+const uint32_t TX_ID_GROUP2 = 0x1FF;
+
+RobomasData om_rm_data_init() {
+  RobomasData data;
+  data.angle = 0;
+  data.rpm = 0;
+  data.current = 0;
+  data.temp = 0;
+  return data;
+}
+
+void om_rm_data_parse(RobomasData* data, const uint8_t raw[8]) {
+  data->angle = (uint16_t)raw[0] | ((uint16_t)raw[1] << 8);
+  data->rpm = (int16_t)raw[2] | ((int16_t)raw[3] << 8);
+  data->current = (int16_t)raw[4] | ((int16_t)raw[5] << 8);
+  data->temp = raw[6];
+}
+
+RobomasCore om_rm_core_init() {
+  RobomasCore core;
+  for (int i = 0; i < 8; ++i) {
+    core.data_[i] = om_rm_data_init();
+  }
+  core.max_output_ = 16384;
+  memset(core.output_, 0, sizeof(core.output_));
+  return core;
+}
+
+void om_rm_core_set_max_output(RobomasCore* core, int16_t max) {
+  core->max_output_ = max > 0 ? max : -max;
+}
+
+int om_rm_core_parse(RobomasCore* core, uint32_t id, const uint8_t data[8]) {
+  if (id < RX_ID_MIN || id > RX_ID_MAX) {
+    return -1;
+  }
+  int index = id - RX_ID_MIN;
+  om_rm_data_parse(&core->data_[index], data);
+  return index;
+}
+
+void om_rm_core_set_output(RobomasCore* core, int16_t current, int id) {
+  if (id < 1 || id > 8) {
+    return;
+  }
+  int index = id - 1;
+  int16_t clamped_current = current;
+  if (clamped_current > core->max_output_) {
+    clamped_current = core->max_output_;
+  } else if (clamped_current < -core->max_output_) {
+    clamped_current = -core->max_output_;
+  }
+  core->output_[0][index] = clamped_current & 0xFF;
+  core->output_[1][index] = (clamped_current >> 8) & 0xFF;
+}
+
+void om_rm_core_set_output_percent(RobomasCore* core, float percent, int id) {
+  if (id < 1 || id > 8) {
+    return;
+  }
+  int16_t current = (int16_t)(percent * core->max_output_);
+  om_rm_core_set_output(core, current, id);
+}
+
+void om_rm_core_get_output(const RobomasCore* core, uint8_t out[2][8]) {
+  memcpy(out, core->output_, sizeof(core->output_));
+}
+
+void om_rm_core_get_output_group(const RobomasCore* core, uint8_t out[8], unsigned int group) {
+  if (group > 1) {
+    memset(out, 0, 8);
+    return;
+  }
+  memcpy(out, core->output_[group], 8);
+}
+
+int16_t om_rm_core_get_current(const RobomasCore* core, int id) {
+  if (id < 1 || id > 8) {
+    return 0;
+  }
+  int index = id - 1;
+  return core->data_[index].current;
+}
+
+uint16_t om_rm_get_angle(const RobomasCore* core, int id) {
+  if (id < 1 || id > 8) {
+    return 0;
+  }
+  int index = id - 1;
+  return core->data_[index].angle;
+}
+
+int16_t om_rm_get_rpm(const RobomasCore* core, int id) {
+  if (id < 1 || id > 8) {
+    return 0;
+  }
+  int index = id - 1;
+  return core->data_[index].rpm;
+}
+
+uint8_t om_rm_get_temp(const RobomasCore* core, int id) {
+  if (id < 1 || id > 8) {
+    return 0;
+  }
+  int index = id - 1;
+  return core->data_[index].temp;
+}
+
+RobomasData om_rm_core_get_data(const RobomasCore* core, int id) {
+  if (id < 1 || id > 8) {
+    return om_rm_data_init();
+  }
+  int index = id - 1;
+  return core->data_[index];
+}
