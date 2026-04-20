cf07f26555ec67a33045f889dbab11cd8735bd8b
bit <mushbeen25@gmail.com>
2026-04-20 15:44:39 +0900

    feat: add mecanum C++ wrapper

--- include/omuraisu/c/chassis/mecanum.h
+++ include/omuraisu/c/chassis/mecanum.h
@@ -3,6 +3,9 @@
 
 #include "coordinate/coordinate.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 typedef struct {
   CoordinatePolar wheel_pos[4];
 } Mecanum;
@@ -15,4 +18,7 @@ Mecanum om_mecanum_init_polar(const CoordinatePolar pos[4]);
 
 void om_mecanum_calc(const Mecanum* self, const Velocity* vel, float result[4]);
 
+#ifdef __cplusplus
+}  // extern "C"
+#endif
 #endif
